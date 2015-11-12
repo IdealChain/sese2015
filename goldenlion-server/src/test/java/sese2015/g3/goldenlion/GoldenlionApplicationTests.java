e6ec6945ff00788cb66f0f74d9e5e6c5549374d0
Mario Luef <mario.luef@extern.brz.gv.at>
2015-11-12 16:44:47 +0100

    Initial commit

---
+++ goldenlion-server/src/test/java/sese2015/g3/goldenlion/GoldenlionApplicationTests.java
@@ -0,0 +1,16 @@
+package sese2015.g3.goldenlion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GoldenlionApplication.class)
+public class GoldenlionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ goldenlion-ui/src/main/resources/WEB-INF/app/index.module.js
@@ -0,0 +1,7 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('goldenlionUi', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
