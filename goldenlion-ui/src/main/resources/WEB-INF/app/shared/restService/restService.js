ad0f4e29f0b7a3159bbe4ed8163423af62a11493
Mario Luef <mario.luef@extern.brz.gv.at>
2015-11-17 22:00:32 +0100

    Read backendlocation from absurl

--- goldenlion-ui/src/main/resources/WEB-INF/app/shared/restService/restService.js
+++ goldenlion-ui/src/main/resources/WEB-INF/app/shared/restService/restService.js
@@ -5,9 +5,9 @@
     .module('goldenlionUi')
     .service('restService', restService);
 
-  function restService($http) {
+  function restService($http, $location) {
     var vm = this;
-    var backend = "http://localhost:8080";	// TODO: Get this parameter from backend
+    var backend = $location.absUrl().substr(0,$location.absUrl().indexOf('#'));	// TODO: Get this parameter from backend
 
     vm.login = function (credentials) {
       return $http({
