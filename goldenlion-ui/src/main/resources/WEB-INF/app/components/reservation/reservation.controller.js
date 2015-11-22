2c301d6239e33b8192cc1bbf724e4fe47dda807b
Fabian Th <fabthe@gmail.com>
2015-11-22 19:32:24 +0100

    basic routing for reservation + add-reservation. Added empty Controller

---
+++ goldenlion-ui/src/main/resources/WEB-INF/app/components/reservation/reservation.controller.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('goldenlionUi')
+    .controller('ReservationController', ReservationController);
+
+  /** @ngInject */
+  function ReservationController() {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+    }
+
+  }
+})();
--- goldenlion-ui/src/main/resources/WEB-INF/app/index.route.js
+++ goldenlion-ui/src/main/resources/WEB-INF/app/index.route.js
@@ -23,17 +23,40 @@
           label: 'Übersicht'
         },
         authenticate: true
-      }).state('customer', {
-      url: '/customer',
-      templateUrl: 'app/components/customer/customer.html',
-      controller: 'CustomerController',
-      controllerAs: 'customerCtrl',
-      ncyBreadcrumb: {
-        label: 'Kunde',
-        parent: 'home'
-      },
-      authenticate: true
-    });
+      })
+      .state('customer', {
+        url: '/customer',
+        templateUrl: 'app/components/customer/customer.html',
+        controller: 'CustomerController',
+        controllerAs: 'customerCtrl',
+        ncyBreadcrumb: {
+          label: 'Kunde',
+          parent: 'home'
+        },
+        authenticate: true
+      })
+      .state('reservation', {
+        url: '/reservation',
+        templateUrl: 'app/components/reservation/reservation.html',
+        controller: 'ReservationController',
+        controllerAs: 'reservationCtrl',
+        ncyBreadcrumb: {
+          label: 'Reservierungen',
+          parent: 'home'
+        },
+        authenticate: true
+      })
+      .state('addreservation', {
+        url: '/reservation/addreservation',
+        templateUrl: 'app/components/reservation/addreservation.html',
+        controller: 'ReservationController',
+        controllerAs: 'reservationCtrl',
+        ncyBreadcrumb: {
+          label: 'Reservierung hinzufügen',
+          parent: 'reservation'
+        },
+        authenticate: true
+      });
     $urlRouterProvider.otherwise('/');
   }
 })();
