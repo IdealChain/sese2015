1b7f6ccffea281f2e335fc6007a4e847e2f6279d
Fabian Th <fabthe@gmail.com>
2015-11-22 13:30:11 +0100

    remove unnecessary defintions

--- goldenlion-server/src/main/java/sese2015/g3/goldenlion/customer/repository/CustomerRepository.java
+++ goldenlion-server/src/main/java/sese2015/g3/goldenlion/customer/repository/CustomerRepository.java
@@ -6,11 +6,5 @@ import sese2015.g3.goldenlion.customer.domain.Customer;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    Customer save(Customer c);
-
-    Customer findOne(Long id);
-
-    Iterable<Customer> findAll(Iterable<Long> iterable);
-
-    Iterable<Customer> findAll();
+    
 }
