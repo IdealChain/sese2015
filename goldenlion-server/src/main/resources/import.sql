7f4f43eca687d9ffe23e39ceb1a19c152d6aa4d6
Fabian Th <fabthe@gmail.com>
2015-11-23 03:02:09 +0100

    added birthday to test data

--- goldenlion-server/src/main/resources/import.sql
+++ goldenlion-server/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO ROOMS (MAX_PERSONS, ROOM_NUMBER) VALUES (2, '11'), (2, '12'), (2, '13'), (3, '14'), (2, '22'), (2, '23'), (3, '23'), (4, 'Royal Suite');
 
 -- http://www.fakenamegenerator.com/gen-random-gr-gr.php
-INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL) VALUES ('Sarah', 'Schneider', 'FEMALE', 'sarahschneider@example.com');
-INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL) VALUES ('Tobias', 'Schreiber', 'MALE', 'tschreiber@example.com');
-INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL) VALUES ('Dennis', 'Sommer', 'MALE', 'dennis87@example.com');
-INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL) VALUES ('Anke', 'Werner', 'FEMALE', 'anke@example.com');
+INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL, BIRTHDAY) VALUES ('Sarah', 'Schneider', 'FEMALE', 'sarahschneider@example.com', '1974-02-14');
+INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL, BIRTHDAY) VALUES ('Tobias', 'Schreiber', 'MALE', 'tschreiber@example.com', '1965-03-28');
+INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL, BIRTHDAY) VALUES ('Dennis', 'Sommer', 'MALE', 'dennis87@example.com', '1991-08-12');
+INSERT INTO CUSTOMERS (FIRST_NAME, LAST_NAME, GENDER, EMAIL, BIRTHDAY) VALUES ('Anke', 'Werner', 'FEMALE', 'anke@example.com', '1964-12-30');
