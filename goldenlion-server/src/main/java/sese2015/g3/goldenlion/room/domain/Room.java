5e5544c47d5029cb30369d4de4a4103cdfeadc4b
Fabian Th <fabthe@gmail.com>
2015-12-08 02:42:36 +0100

    Room-domain: added missing props; basic frontend-ui /wo functionality

--- goldenlion-server/src/main/java/sese2015/g3/goldenlion/room/domain/Room.java
+++ goldenlion-server/src/main/java/sese2015/g3/goldenlion/room/domain/Room.java
@@ -2,6 +2,7 @@ package sese2015.g3.goldenlion.room.domain;
 
 import sese2015.g3.goldenlion.commons.entity.PersistentObject;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -15,6 +16,18 @@ public class Room extends PersistentObject {
 
     @NotNull
     private int maxPersons;
+
+    private Double price1P;
+
+    private Double price2P;
+
+    private Double price3P;
+
+    private Double price1P1K;
+
+    private Double price1P2K;
+
+    private Double price2P1K;
 
     public Room() {
 
@@ -36,5 +49,51 @@ public class Room extends PersistentObject {
         this.maxPersons = maxPersons;
     }
 
-    //TODO implement prices per 1, 2, 3 persons + 1, 2 kids
+    public Double getPrice1P() {
+        return price1P;
+    }
+
+    public void setPrice1P(Double price1P) {
+        this.price1P = price1P;
+    }
+
+    public Double getPrice2P() {
+        return price2P;
+    }
+
+    public void setPrice2P(Double price2P) {
+        this.price2P = price2P;
+    }
+
+    public Double getPrice3P() {
+        return price3P;
+    }
+
+    public void setPrice3P(Double price3P) {
+        this.price3P = price3P;
+    }
+
+    public Double getPrice1P1K() {
+        return price1P1K;
+    }
+
+    public void setPrice1P1K(Double price1P1K) {
+        this.price1P1K = price1P1K;
+    }
+
+    public Double getPrice1P2K() {
+        return price1P2K;
+    }
+
+    public void setPrice1P2K(Double price1P2K) {
+        this.price1P2K = price1P2K;
+    }
+
+    public Double getPrice2P1K() {
+        return price2P1K;
+    }
+
+    public void setPrice2P1K(Double price2P1K) {
+        this.price2P1K = price2P1K;
+    }
 }
--- goldenlion-ui/src/main/resources/WEB-INF/app/components/room/room.controller.js
+++ goldenlion-ui/src/main/resources/WEB-INF/app/components/room/room.controller.js
@@ -6,6 +6,21 @@
     .controller('RoomController', RoomController);
 
   /** @ngInject */
-  function RoomController() {
+  function RoomController(restService) {
+    var vm = this;
+    vm.freeRoomsOnly = false;
+    vm.startDate = new Date();
+    vm.endDate = new Date();
+    vm.endDate.setDate(vm.startDate.getDate() + 7);
+
+    restService.allRooms().then(
+      function successCallback(response) {
+        vm.rooms = response.data;
+        console.log(vm.rooms);
+      },
+      function errorCallback(response) {
+        alert('Error: Could not receive room data');
+      }
+    );
   }
 })();
