a686083e4e2d0fca084b4b4ee6ca49dc8cae2e23
Daniel Achleitner <daniel.achleitner@gmail.com>
2015-12-11 09:13:29 +0100

    basic room details

--- goldenlion-ui/src/main/resources/WEB-INF/app/components/room/roomdetail.controller.js
+++ goldenlion-ui/src/main/resources/WEB-INF/app/components/room/roomdetail.controller.js
@@ -6,7 +6,21 @@
     .controller('RoomDetailController', RoomDetailController);
 
   /** @ngInject */
-  function RoomDetailController(restService) {
+  function RoomDetailController(restService, $stateParams) {
+    var vm = this;
+    vm.roomid = $stateParams.roomid;
 
+    // fetch room data
+    restService.roomById(vm.roomid).then(
+      function successCallback(response) {
+        vm.room = response.data;
+      },
+      function errorCallback() {
+        alert('Error: Could not receive room data');
+      }
+    );
+
+    // fetch room reservations
+    // TODO
   }
 })();
