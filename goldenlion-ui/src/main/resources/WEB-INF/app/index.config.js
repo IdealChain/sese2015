90ef3cf4b65a17642e2d5693b2acd72ad86c1e1b
Daniel Achleitner <daniel.achleitner@gmail.com>
2015-12-15 21:52:21 +0100

    fix breadcrumb display

--- goldenlion-ui/src/main/resources/WEB-INF/app/index.config.js
+++ goldenlion-ui/src/main/resources/WEB-INF/app/index.config.js
@@ -7,7 +7,7 @@
     .config(function ($breadcrumbProvider) {
       $breadcrumbProvider.setOptions({
         prefixStateName: 'home',
-        template: '<div style="padding-left: 1em; padding-top: 1em"> <span class="md-title" ng-repeat="step in steps" ng-class="{active: $last}" ng-switch="$last || !!step.abstract"> <a ng-switch-when="false" href="{{step.ncyBreadcrumbLink}}"> {{step.ncyBreadcrumbLabel}} </a> <span class="md-title" ng-switch-when="false"> > </span> <span class="md-title" ng-switch-when="true">{{step.ncyBreadcrumbLabel}} </span> </span> </div>'
+        template: '<div style="padding-left: 1em; padding-top: 1em"> <span class="md-title" ng-repeat="step in steps" ng-class="{active: $last}" ng-switch="$last || !!step.abstract"> <a ng-switch-when="false" href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a> <span class="md-title" ng-switch-when="false"> > </span> <span class="md-title" ng-switch-when="true">{{step.ncyBreadcrumbLabel}} </span> </span> </div>'
       });
     })
     .config(function (paginationTemplateProvider) {
@@ -16,8 +16,8 @@
     .
     config(function ($httpProvider) {
       $httpProvider.interceptors.push('authInterceptor');
-    })
-  
+    });
+
   angular.isUndefinedOrNull = function (val) {
     return angular.isUndefined(val) || val === null
   };
